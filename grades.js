01c88519b11e52da130f830dc05f615f14dd9f58
Joey <joeybb@MacBook-Pro.local>
2017-04-09 22:13:30 -0500

    first commit

---
+++ grades.js
@@ -0,0 +1,13 @@
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+for (var i = 0; i < scores.length; i++) {
+	console.log("List of grades",scores[i]);
+}
+
+console.log("The number of grades are ", scores.length);
+
+var lowest = Math.min(82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87);
+console.log("The lowest grade is ", lowest);
+
+var highest = Math.max(82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87);
+console.log("The highest grade is ", highest);
